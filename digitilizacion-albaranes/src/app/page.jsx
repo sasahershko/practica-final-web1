0b046dd7769ed49393a9a3a5653cdd9ae3560586
sasahershko <sarayhershko@gmail.com>
2024-11-20 12:20:18 +0100

    Update page.jsx

--- digitilizacion-albaranes/src/app/page.jsx
+++ digitilizacion-albaranes/src/app/page.jsx
@@ -3,11 +3,11 @@
 
 export default function HomePage(){
     return (
-       <div className  ='flex flex-col items-center justify-center bg-white text-black mt-[200px]'>
+       <div className  ='flex flex-col items-center justify-center bg-white text-black mt-[260px]'>
 
-            <h1 className='animate-fade-in-up text-7xl font-bold '>Welcome to</h1>
+            <h1 className='animate-fade-in-up text-7xl font-bold w-full text-center'>Welcome to</h1>
 
-            <h2 className='text-9xl font-semibold mt-4 animate-fade-in-down'>
+            <h2 className='text-9xl font-semibold mt-4 animate-fade-in-down w-full text-center'>
                 {/* bg-clip es para que se añada solo al texto y no al fondo */}
                 <span className='bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500 bg-clip-text text-transparent animate-gradient-move'>
                     Digitization of delivery notes
