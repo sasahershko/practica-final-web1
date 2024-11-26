00adc67b8c753a06f2db67da7ecdf186fb6a7900
sasahershko <sarayhershko@gmail.com>
2024-11-26 21:50:11 +0100

    Update NavBarDashboard.jsx

--- digitilizacion-albaranes/src/app/components/NavBarDashboard.jsx
+++ digitilizacion-albaranes/src/app/components/NavBarDashboard.jsx
@@ -1,10 +1,9 @@
 'use client'
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { Button, user, User } from '@nextui-org/react';
 import Link from 'next/link';
-import LogoutButton from '../components/LogoutButton';
 import SearchBar from './SearchBar';
+import {logout} from '../lib/logout';
 
 export default function NavBar() {
     const router = useRouter();
@@ -37,6 +36,14 @@ export default function NavBar() {
 
     }, []);
 
+    const handleLogout = async() =>{
+        try{
+            await logout();
+        }catch(error){
+            console.log(error.message);
+        }
+    }
+
 
     return (
         <>
@@ -54,15 +61,20 @@ export default function NavBar() {
 
                     {isAuthenticated ? (
                         <div className='flex gap-4 mt-3'>
-                            {/* <User 
-                                name='Sasa Lara' 
-                                avatarProps={{
-                                    src: 'https://gcs.tripi.vn/public-tripi/tripi-feed/img/474230Lgd/hinh-chibi-avatar-dep_031501308.jpg'
-                                }}
-                                className='text-black'/> */}
                             <div className="relative">
+   
                                 <button
-                                    onClick={() => setUserMenuOpen((prev) => !prev)}
+                                    onClick={(e) => {
+                                        e.stopPropagation(); //evita que el evento cierre al momento
+                                        setUserMenuOpen((prev) => !prev); 
+                                        if (!userMenuOpen) {
+                                            document.addEventListener(
+                                                'click',
+                                                () => setUserMenuOpen(false),
+                                                { once: true } //cierra con un clic fuera, solo una vez
+                                            );
+                                        }
+                                    }}
                                     className="relative z-auto w-10 h-10 rounded-full overflow-hidden focus:outline-none ring-2 ring-blue-500 transform transition-transform duration-200 hover:scale-105"
                                     aria-label="User menu"
                                 >
@@ -72,36 +84,35 @@ export default function NavBar() {
                                         className="object-cover w-full h-full"
                                     />
                                 </button>
+                                <p className='text-black'>User</p>
 
                                 <div
                                     className={`absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg z-20 transform transition-all duration-300 origin-top-right ${userMenuOpen
-                                            ? 'scale-100 opacity-100 translate-y-0'
-                                            : 'scale-95 opacity-0 -translate-y-2 pointer-events-none'
+                                        ? 'scale-100 opacity-100 translate-y-0'
+                                        : 'scale-95 opacity-0 -translate-y-2 pointer-events-none'
                                         }`}
                                 >
                                     <Link
-                                        href="/profile"
+                                        href="/"
                                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition duration-150"
                                     >
                                         Profile
                                     </Link>
                                     <Link
-                                        href="/settings"
+                                        href="/pages/dashboard/settings"
                                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition duration-150"
                                     >
                                         Settings
                                     </Link>
                                     <Link
-                                        href="/logout"
+                                        href="/"
                                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition duration-150"
+                                        onClick={() =>{handleLogout()}}
                                     >
                                         Logout
                                     </Link>
                                 </div>
                             </div>
-
-
-                            {/* <LogoutButton /> */}
                         </div>
                     ) : (
                         null
