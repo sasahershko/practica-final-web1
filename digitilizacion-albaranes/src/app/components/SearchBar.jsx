250f2d98358a9f2865c087c02954a5a016d28588
sasahershko <sarayhershko@gmail.com>
2024-11-28 09:48:07 +0100

    he corregido cosas

--- digitilizacion-albaranes/src/app/components/SearchBar.jsx
+++ digitilizacion-albaranes/src/app/components/SearchBar.jsx
@@ -14,7 +14,7 @@ export default function SearchBar({onSearch}){
         <div className='relative w-full max-w-lg text-black'>
             <input
                 type= 'text'
-                placeHolder= 'Search...'
+                placeholder= 'Search...'
                 value={search}
                 onChange={handleSearch}
                 className='w-full pl-10 pr-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-blue-300 focus:border-transparent bg-gray-100 transition-all duration-200 focus:bg-white' 
