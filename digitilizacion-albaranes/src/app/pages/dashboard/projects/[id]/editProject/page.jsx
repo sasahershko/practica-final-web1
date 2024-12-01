5361e03e6f7bab13cc03f14a0ac1fd9eb7413f89
sasahershko <sarayhershko@gmail.com>
2024-12-01 19:43:49 +0100

    Cambios en proyectos. He quitado modal, para poder ver los albaranes en su pág

--- digitilizacion-albaranes/src/app/pages/dashboard/projects/[id]/editProject/page.jsx
+++ digitilizacion-albaranes/src/app/pages/dashboard/projects/[id]/editProject/page.jsx
@@ -1,100 +1,53 @@
 'use client';
 import { useParams, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useProjectDetails } from '@/app/hooks/useProjectDetails';
 import ProjectForm from '@/app/pages/dashboard/projects/components/ProjectForm';
+import { deleteProject, updateProject } from '@/app/lib/projects';
 
-///quiero info del proyexto y del cliente
-import { getProjectById, deleteProject, updateProject } from '@/app/lib/projects';
-import { getClientById } from '@/app/lib/clients';
+export default function ProjectDetails() {
+  const { id } = useParams();
+  const router = useRouter();
+  const { project, client, error, loading } = useProjectDetails(id);
 
-export default function ProjectDetails(){
-    const {id} = useParams();
-    const router = useRouter();
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    const [project, setProject] = useState(null);
-    const [client, setClient] = useState(null);
-    const [clientId, setClientId] = useState(null);
-    
-    useEffect(() => {
-      const fetchProject = async () => {
-        try {
-          const projectData = await getProjectById(id);
-          setProject({
-            name: projectData.name || '',
-            email: projectData.email || '',
-            projectCode: projectData.projectCode || '',
-            street: projectData.address?.street || '',
-            number: projectData.address?.number || '',
-            postal: projectData.address?.postal || '',
-            city: projectData.address?.city || '',
-            province: projectData.address?.province || '',
-            code: projectData.code || '',
-            clientId: projectData.clientId || '',
-            notes: projectData.notes || '',
-          });
-
-          //para separarlo del fetch del project
-          setClientId(projectData.clientId || null);
-
-        } catch (error) {
-          setError(error.message);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchProject();
-    }, [id]);
-  
-
-    useEffect(() => {
-      if (clientId) {
-        const fetchClient = async () => {
-          try {
-            const clientData = await getClientById(clientId);
-            setClient(clientData);
-          } catch (error) {
-            setError(error.message);
-          }
-        };
-  
-        fetchClient();
-      }
-    }, [clientId]);
-
-    const handleDelete  = async() =>{
-      try{
-        const response = await deleteProject(id);
-        alert('Project deleted');
-        router.push('/pages/dashboard/projects');
-      }catch(error){
-        console.error(error.message);
-        setError(error.message);
-      }
+  const handleDelete = async () => {
+    try {
+      await deleteProject(id);
+      alert('Project deleted');
+      router.push('/pages/dashboard/projects');
+    } catch (error) {
+      console.error(error.message);
     }
+  };
 
-    const handleUpdate = async(values) =>{
-      try{
-        const response = await updateProject(id, values);
-        alert('Project updated');
-      }catch(error){
-        console.error(error.message);
-        setError(error.message);
-      }
+  const handleUpdate = async (values) => {
+    try {
+      await updateProject(id, values);
+      alert('Project updated');
+    } catch (error) {
+      console.error(error.message);
     }
+  };
 
-    return(
-        <div className="p-8">
-        <ProjectForm
-          initialValues={project} 
-          onSubmit={handleUpdate}
-          title="Edit Project" 
-          isEdit={true}
-          onDelete={handleDelete}
-          client={client}
-        />
+  if (loading){
+    return (
+      <div className="flex justify-center items-center h-[400px]">
+        <div className="loader border-t-4 border-blue-500 rounded-full w-12 h-12 animate-spin"></div>
       </div>
-    )
+    );
+  } 
+
+  // if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div className="p-8">
+      <ProjectForm
+        initialValues={project}
+        onSubmit={handleUpdate}
+        title="Edit Project"
+        isEdit={true}
+        onDelete={handleDelete}
+        client={client}
+      />
+    </div>
+  );
 }
--- digitilizacion-albaranes/src/app/pages/dashboard/projects/[id]/page.jsx
+++ digitilizacion-albaranes/src/app/pages/dashboard/projects/[id]/page.jsx
@@ -1,30 +1,101 @@
 'use client';
 import { useParams, useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { getProjectById } from '@/app/lib/projects';
+import { useProjectDetails } from '@/app/hooks/useProjectDetails';
+import Card from '@/app/components/Card';
 
 export default function Home() {
-    const {id} = useParams();
-    const [project, setProject] = useState(null);
+    const { id } = useParams();
+    const { project, loading, error, client } = useProjectDetails(id);
+    const router = useRouter();
 
-    useEffect(() => {
-        const fetchProject = async() =>{
-            try{
-                const result = await getProjectById(id);
-                setProject(result);
-            }catch(e){
-                console.log(e);
-            }
-        };
-
-        fetchProject();
-        console.log(project);
-    }, []);
+    if (loading) {
+        return (
+            <div className='flex justify-center items-center h-screen'>
+                <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500'></div>
+            </div>
+        );
+    }
+    if (error) return <p>Error: {error}</p>;
 
     return (
-        <div>
-        <h1 className='text-center text-black text-[80px] animate-fade-in-up font-bold'>a</h1>
+        <div className='p-8 space-y-8'>
+            <button className='blue-button' onClick={()=>router.push('/pages/dashboard/projects')}>Go back to Projects</button>
+            <h1 className='bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500 bg-clip-text text-transparent animate-gradient-move text-center text-[100px] font-bold'>
+                {project?.name || 'No Project Found'}
+            </h1>
+            <div className='grid grid-cols-3 gap-8 text-black'>
+                <div className='col-span-2'>
+                    <Card title={<span className='text-[30px]'>{`Project Details`}</span>}>
+                        <div className='grid grid-cols-2 gap-4'>
+                            <div>
+                                <label className='block text-gray-500 font-medium'>Project Code</label>
+                                <p className='bg-gray-100 p-2 rounded-lg'>{project?.projectCode || 'N/A'}</p>
+                            </div>
+                            <div>
+                                <label className='block text-gray-500 font-medium'>Code</label>
+                                <p className='bg-gray-100 p-2 rounded-lg'>{project?.code || 'N/A'}</p>
+                            </div>
+                            <div className='col-span-2'>
+                                <label className='block text-gray-500 font-medium'>Notes</label>
+                                <p className='bg-gray-100 p-2 rounded-lg'>{project?.notes || 'N/A'}</p>
+                            </div>
+                            <div className='col-span-2'>
+                                <label className='block text-gray-500 font-medium'>Address</label>
+                                <p className='bg-gray-100 p-2 rounded-lg'>
+                                    {`${project?.street || 'N/A'}, ${project?.number || 'N/A'}, 
+                                    ${project?.postal || 'N/A'}, ${project?.city || 'N/A'}, 
+                                    ${project?.province || 'N/A'}`}
+                                </p>
+                            </div>
+                        </div>
+                        <div className='mt-6 text-right'>
+                            <button className='bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600'
+                                onClick={() => router.push(`/pages/dashboard/projects/${id}/editProject`)}>
+                                Edit
+                            </button>
+                        </div>
+                    </Card>
+                </div>
+
+
+                <div className='col-span-1'>
+                    <Card title={<span className='text-[30px]'>{`Client Details`}</span>}>
+                        <div className='space-y-4 text-black'>
+                            <div>
+                                <label className='block text-gray-500 font-medium'>Name</label>
+                                <p className='bg-gray-100 p-2 rounded-lg'>{client?.name || 'N/A'}</p>
+                            </div>
+                            <div>
+                                <label className='block text-gray-500 font-medium'>CIF</label>
+                                <p className='bg-gray-100 p-2 rounded-lg'>{client?.cif || 'N/A'}</p>
+                            </div>
+                            <div>
+                                <label className='block text-gray-500 font-medium'>Address</label>
+                                <p className='bg-gray-100 p-2 rounded-lg'>
+                                    {`${client?.address?.street || 'N/A'}, ${client?.address?.number || 'N/A'}, 
+                                    ${client?.address?.postal || 'N/A'}, ${client?.address?.city || 'N/A'}, 
+                                    ${client?.address?.province || 'N/A'}`}
+                                </p>
+                            </div>
+                            {client?.logo && (
+                                <div>
+                                    <label className='block text-gray-500 font-medium'>Logo</label>
+                                    <img
+                                        src={client.logo}
+                                        alt={`${client.name} logo`}
+                                        className='w-24 h-auto mt-2 rounded-md'
+                                    />
+                                </div>
+                            )}
+                        </div>
+                    </Card>
+                </div>
+            </div>
+            <Card title={<span className="text-[30px]">{`Delivery Notes`}</span>}>
+                <div className="space-y-4">
+                    <p className="border border-blue-300 p-2 rounded-lg text-blue-950">No delivery notes available.</p>
+                </div>
+            </Card>
         </div>
     );
 }
